1bc7fe651deb3eead7d9d99d7dd675c0d8d2cca3
RiteshRana25 <160099737+RiteshRana25@users.noreply.github.com>
2026-01-09 18:58:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// backend/index.js
+
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
@@ -8,22 +8,17 @@ dotenv.config();
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
 connectDB();
 
-// Routes
 app.use("/api/images", require("./routes/images"));
 
-// Default route
 app.get("/", (req, res) => {
   res.send("✅ Uploader API is running");
 });
 
-// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
